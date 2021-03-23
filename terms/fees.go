25551874099bdfa41f992862abd983236fa20e94
Johan T. Halseth <johanth@gmail.com>
2021-03-23 14:40:07 +0100

    add serialization of fee schedule

--- terms/fees.go
+++ terms/fees.go
@@ -1,6 +1,12 @@
 package terms
 
-import "github.com/btcsuite/btcutil"
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"github.com/btcsuite/btcutil"
+)
 
 // FeeSchedule is an interface that represents the configuration source that
 // the auctioneer will use to determine how much to charge in fees for each
@@ -13,7 +19,15 @@ type FeeSchedule interface {
 	// ExecutionFee computes the execution fee (usually based off of a
 	// rate) for the target amount.
 	ExecutionFee(amt btcutil.Amount) btcutil.Amount
+
+	Serialize(w io.Writer) error
 }
+
+type feeScheduleType uint8
+
+const (
+	typeLinearFeeSchedule feeScheduleType = 0
+)
 
 // LinearFeeSchedule is a FeeSchedule that calculates the execution fee based
 // upon a static base fee and a variable fee rate in parts per million.
@@ -41,6 +55,59 @@ func (s *LinearFeeSchedule) FeeRate() btcutil.Amount {
 // NOTE: This method is part of the orderT.FeeSchedule interface.
 func (s *LinearFeeSchedule) ExecutionFee(amt btcutil.Amount) btcutil.Amount {
 	return amt * s.feeRate / 1_000_000
+}
+
+// Serialize
+//
+// NOTE: This method is part of the orderT.FeeSchedule interface.
+func (s *LinearFeeSchedule) Serialize(w io.Writer) error {
+	err := binary.Write(w, binary.BigEndian, typeLinearFeeSchedule)
+	if err != nil {
+		return err
+	}
+
+	err = binary.Write(w, binary.BigEndian, int64(s.baseFee))
+	if err != nil {
+		return err
+	}
+
+	err = binary.Write(w, binary.BigEndian, int64(s.feeRate))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeserializeFeeSchedule(r io.Reader) (FeeSchedule, error) {
+	var t feeScheduleType
+	err := binary.Read(r, binary.BigEndian, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case typeLinearFeeSchedule:
+	default:
+		return nil, fmt.Errorf("unknown fee schedule type %v", t)
+	}
+
+	var baseFee, feeRate int64
+	err = binary.Read(r, binary.BigEndian, &baseFee)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Read(r, binary.BigEndian, &feeRate)
+	if err != nil {
+		return nil, err
+	}
+
+	s := NewLinearFeeSchedule(
+		btcutil.Amount(baseFee), btcutil.Amount(feeRate),
+	)
+
+	return s, nil
 }
 
 // NewLinearFeeSchedule creates a new linear fee schedule based upon a static
